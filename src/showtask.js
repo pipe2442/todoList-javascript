7ec25052ba3b27030dd56dba4afb91560cf48ae6
carloso0114 <carlossospinag@gmail.com>
2021-06-18 16:29:00 -0500

    add information to tasks

--- src/showtask.js
+++ src/showtask.js
@@ -1,7 +1,5 @@
 import { projectList } from "./newproject";
 
-
-
 const showTaskList = () => {
     const id2Selector = createTask.getAttribute('id2')
     const array = projectList[id2Selector].todolist
@@ -9,10 +7,10 @@ const showTaskList = () => {
     list.innerHTML = "";
     array.forEach(function(array) {
         list.innerHTML += `
-        <h5>${array.title}</h5>
-        <h5>${array.description}</h5>
-        <h5>${array.dueDate}</h5>
-        <h5>${array.priority}</h5>
+        <h5>Title: ${array.title}</h5>
+        <h5>Description: ${array.description}</h5>
+        <h5>Due Date: ${array.dueDate}</h5>
+        <h5>Priority: ${array.priority}</h5>
         `
         finalTasklist.append(list)
     });
