e4472412d9d6f0b443da398ce2f8b6c54ce9ad4c
pipe2442 <felipeangulo2442@gmail.com>
2021-06-21 17:40:26 -0500

    Create delete function

--- src/showtask.js
+++ src/showtask.js
@@ -8,6 +8,7 @@ const showTaskList = () => {
     let taskindex = 0
     const list = document.getElementById('taskqueue')
     list.innerHTML = "";
+
     task.forEach(function (task) {
         list.innerHTML += `
         <h5 class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample${taskindex}" role="button"
@@ -20,14 +21,48 @@ const showTaskList = () => {
             <h5>Description: ${task.description}</h5>
             <h5>Due Date: ${task.dueDate}</h5>
             <h5>Priority: ${task.priority}</h5>
+            <button type="button" class="btn btn-danger" id="t${taskindex}">Delete</button>
             </div>
         </div>
     </div>
-    `
+    `   
         finalTasklist.append(list)
+        // const deletebuttonfinal = document.querySelectorAll('#taskqueue div div div button')
+        // console.log(deletebuttonfinal)
+        
+        // const giveidbtn = document.getElementById('deletebtntest')
+        // giveidbtn.setAttribute('id2', taskArr)
+
+        
+        
+
+            // deletebuttonfinal.addEventListener('click', () => { 
+            //     projectList[id2Selector].todolist.splice(taskindex, 1);
+            // }); 
+        // deletebuttonfinal.forEach(function (t) {
+        //     deletebuttonfinal.addEventListener('click', () => { 
+        //         projectList[id2Selector].todolist.splice(taskindex, 1);
+        //     });            
+        // })
+
         taskindex++
+        // const deletebuttonselector = document.querySelectorAll("#deletetaskbutton");
+
+       
+        
     });
+    let deletebuttonfinal = document.querySelectorAll('#taskqueue div div div button')
+    //const deletebuttonfinal = document.getElementById(taskindex)
+    console.log(deletebuttonfinal)
+
+
 }
+        //deletebuttonfinal.forEach(function (t) {
+            // deletebuttonfinal.addEventListener('click', () => { 
+            //     projectList[id2Selector].todolist.splice(taskindex, 1);
+            // });            
+        //})
+
 
 const wipeShowTask = () => {
     const list = document.getElementById('taskqueue')
