35bc4ffcd1a33bf0a43c341cda9a8018e4c4bbd4
carloso0114 <carlossospinag@gmail.com>
2021-06-23 17:04:17 -0500

    clean localstorage

--- src/localstorage.js
+++ src/localstorage.js
@@ -3,23 +3,16 @@ import {
 } from './newproject';
 const save_localstorage = () => {
     localStorage.setItem('ListaDeProyectos', JSON.stringify(projectList))
-
 }
 
 const get_localstorage = () => {
     
     if (localStorage.getItem('ListaDeProyectos')) {
         let test = JSON.parse(localStorage.getItem('ListaDeProyectos')) 
-        console.log('si existe')
         test.forEach(t => {
             projectList.push(t)
             projectDomLocal(t)
-        console.log(t)
         });
-
-
-    }else {
-        console.log('no existe')
     }
 }
 
